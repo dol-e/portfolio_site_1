b3bd1b886f562ccf19dded7fdaa73f57183e32da
mira-yoon2 <amorfati823@gmail.com>
2022-10-10 23:09:39 +0900

    모바일메뉴완성

--- js/header.js
+++ js/header.js
@@ -1,21 +1,43 @@
+
+
+/* 메뉴 - PC버전 */
 const wrap = document.querySelector("#wrap");
 const gnb = wrap.querySelector("#gnb");
 const gnb_lis = wrap.querySelectorAll("#gnb > li");
+
+
+//서브메뉴 배경 넣기
 const subMenuBg = document.createElement('div');
 subMenuBg.className="sub_bg";
 wrap.appendChild(subMenuBg);
 
 
-function mouseOverHandler(){
+//서브메뉴가 있으면 li에 클래스 down붙이기
+for(let el of gnb_lis) {
+  const isSubMenu = el.querySelector(".sub_menu");
+  if(isSubMenu) {
+    el.classList.add("down");
+  } 
+}
+
+
+//풀다운메뉴
+
+function mouseOverHandler(e){
+
   for(let el of gnb_lis) {
     el.classList.remove("on");
   }
+
+  const isSubMenu = e.currentTarget.querySelector(".sub_menu");
+  if(!isSubMenu) return;
+
   this.classList.add("on");
   subMenuBg.classList.add("on");
   
 }
 
-function mouseLeaveHandler(){
+function mouseLeaveHandler(e){
   for(let el of gnb_lis) {
     el.classList.remove("on");
   }
@@ -24,9 +46,53 @@ function mouseLeaveHandler(){
 
 for(let el of gnb_lis) {
   el.addEventListener('mouseover',mouseOverHandler);
+  el.addEventListener('mouseleave',mouseLeaveHandler);
 }
 
 gnb.addEventListener('mouseleave',mouseLeaveHandler);
+
+
+
+
+/* 메뉴 - 모바일 버전 */
+const gnb_mo_lis = wrap.querySelectorAll("#gnb_mo > ul > li");
+const gnb_mo_sub = wrap.querySelector("#gnb_mo > ul > li .sub_menu");
+
+
+//서브메뉴가 있으면 li에 클래스 down붙이기
+for(let el of gnb_mo_lis) {
+  const isSubMenu = el.querySelector(".sub_menu");
+  if(isSubMenu) {
+    el.classList.add("down");
+  } 
+}
+
+// 아코디언 메뉴
+function mouseClickHandler(){
+
+  const target = this;
+
+  const isSubMenu = target.querySelector(".sub_menu");
+  if(!isSubMenu) return;
+
+  const hasClassOn = target.classList.contains("on");
+
+  for(let el of gnb_mo_lis) {
+    el.classList.remove("on");
+  }
+
+  if(hasClassOn){
+    target.classList.remove("on");
+  } else {
+    target.classList.add("on");
+  }
+
+}
+
+gnb_mo_lis.forEach(function(el) {
+  el.addEventListener('click',mouseClickHandler);
+});
+
 
 
 
